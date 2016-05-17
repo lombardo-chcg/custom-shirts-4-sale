51844a6a90e4ad85a0ed90336d5397448b64cb4c
Dominick Lombardo <email": null>
2016-05-16 20:06:47 -0500

    Add some ajax work to index page

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,36 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  // show single shirt above index page
+  $('.container').on('click', '.show-shirt-link', function(event) {
+    event.preventDefault();
+    route = ($(this).attr('href'))
+    ajaxRequest = $.ajax({
+                    method: "GET",
+                    url: route
+                  })
+    ajaxRequest.done(function(results) {
+      $('.popup').append(results).show('slow');
+    });
+  })
+
+  // hide show single shirt container on index page
+  $(document).on('click', '#close', function(event) {
+    event.preventDefault();
+    $('.popup').empty().hide('slow');
+  });
+
+  // show add to card page from index page
+  $(document).on('click', '#home-page-cart', function(event) {
+    event.preventDefault();
+    route = $('#home-page-cart').closest('a').attr('href')
+    ajaxRequest = $.ajax({
+                    method: "GET",
+                    url: route
+                  });
+    ajaxRequest.done(function(results) {
+      $('.popup').empty()
+      $('.popup').append(results).show('slow');
+    })
+  })
+
 });
